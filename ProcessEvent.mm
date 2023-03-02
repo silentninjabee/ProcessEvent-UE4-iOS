1fd5bd8cad8e6566914f6510bdc69c900a2c6a75
silentninjabee <91723045+silentninjabee@users.noreply.github.com>
2023-03-02 14:48:44 +1000

    Update ProcessEvent.mm
    Passing func address as a parameter

--- ProcessEvent.mm
+++ ProcessEvent.mm
@@ -9,7 +9,6 @@ long FindFunctionCheckedOffset = 0x0154a6a4;
 //void __thiscall UObject::ProcessEvent(UObject *this,UFunction *param_1,void *param_2)
 long ProcessEventOffset = 0x024b57ec;
 
-
 static void ProcessEvent(long Object, wchar_t* FunctionName, void* params){
     
     long BaseAdress = (long)_dyld_get_image_header(0);
@@ -35,14 +34,76 @@ static void ProcessEvent(long Object, wchar_t* FunctionName, void* params){
     return;
 }
 
-/* example */
-Vector Rotator = {0,0,0};
-ProcessEvent(MyController, L"ClientSetControlRotation", &Rotator);
 
-/* how the structure for params works */
+// Calling using the function hardcoded address, example from a sdk dump
+
+// ProcessEvent can be adapted to use the function address directly, without first using FindFunctionChecked to look up the function by name. 
+// Instead, the address is cast to a function pointer of the correct type and passed directly to ProcessEvent
+// This wrapper function returns a Boolean value indicating whether the event was successfully processed or not.
+
+static bool bProcessEvent(long Object, void* FunctionAddress, void* Params);
+
+static bool bProcessEvent(long Object, void* FunctionAddress, void* Params) {
+    
+    long BaseAddress = (long)_dyld_get_image_header(0);
+    
+    const auto ProcessEventFunction = reinterpret_cast<void*>(BaseAddress + ProcessEventOffset);
+    
+    if (ProcessEventFunction) {
+        
+        // Note that the reinterpret_cast is used to convert the FunctionAddress and Params pointers to long values
+        reinterpret_cast<void(*)(long, long, long)>(ProcessEventFunction)(Object, reinterpret_cast<long>(FunctionAddress), reinterpret_cast<long>(Params));
+        
+        return true;
+    }
+    return false;
+}
+
+
+ 
+
+/*     example   
+Function Name: DungeonAccess();
+Class: uWorld.ShooterPlayerController;
+Params & Returns: 0, 0;
+Offset Address: 0x1044e9650;
+*/
+
+
+// Class address of the function DungeonAccess
+long ShooterPlayerController = 0x12345678;
+
+// no parameters or return value
+void* Params = nullptr;
+
+ProcessEvent(ShooterPlayerController, L"DungeonAccess", &Params);
+
+/*      using the function address to call, with a boolean return    */
+
+// BaseAddr needs to be defined within scope
+long BaseAddr = (long)_dyld_get_image_header(0);
+
+// the address of the function, cast to a void* 
+void* FunctionAddress = reinterpret_cast<void*>(BaseAddr + 0x1044e9650);
+
+void* Params = nullptr;
+
+if(bProcessEvent(MyController, &FunctionAddress, &Params))
+{
+    // Success
+}
+
+
+/* 
+
+How to define structs for function parameters and return values 
+
+*/
+
 
 /*
-- Pretend there is a function called CheckPointsSameLine(isnt a real function)
+- Example use case: 
+Pretend there is a function called CheckPointsSameLine(isnt a real function)
 which is in class AFakeFunctions, has a reutrn type of bool,  a vector, a rotator, and another vector as an argument
 bool AFakeFunctions::CheckPointsSameLine(Vector StartLocation, Rotator AimRotation, Vector TargetLocation);
 and the goal is to make a triggerbot and there is a shoot function 
